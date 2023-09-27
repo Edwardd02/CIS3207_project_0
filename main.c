0ba41ed6dfe1e9722d970a7bea02e9af3ab8cf5b
Edwardd02 <tup19197@temple.edu>
2023-09-27 22:18:46 +0900

    simpler

--- main.c
+++ main.c
@@ -3,8 +3,7 @@
 #include <dirent.h>
 #include <string.h>
 
-// A function to print the contents of a directory, non-recursively.
-void printDir(const char *path) {
+void printDir(const char *path, int recursive) {
     DIR *dir;
     struct dirent *entry;
     struct dirent **namelist;
@@ -16,7 +15,7 @@ void printDir(const char *path) {
         exit(1);
     }
 
-    n = scandir(path, &namelist, NULL, alphasort); // corrected the filter function to NULL
+    n = scandir(path, &namelist, NULL, alphasort);
     if (n == -1) {
         perror("tuls: cannot scan directory");
         exit(1);
@@ -30,48 +29,13 @@ void printDir(const char *path) {
             continue;
         }
 
-        printf("%s\n", entry->d_name);
-        free(namelist[i]);
-    }
-
-    free(namelist);
-    closedir(dir);
-}
-
-// A function to recursively print the contents of a directory.
-void recursivePrint(const char *path) {
-    DIR *dir;
-    struct dirent *entry;
-    struct dirent **namelist;
-    int n;
-
-    dir = opendir(path);
-    if (!dir) {
-        perror("tuls: cannot open directory");
-        exit(1);
-    }
-
-    n = scandir(path, &namelist, NULL, alphasort); // corrected the filter function to NULL
-    if (n == -1) {
-        perror("tuls: cannot scan directory");
-        exit(1);
-    }
-
-    for (int i = 0; i < n; i++) {
-        entry = namelist[i];
-
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
-            free(namelist[i]);
-            continue;
-        }
-
-        char subPath[4096]; // You may need to adjust the size
+        char subPath[4096];
         snprintf(subPath, sizeof(subPath), "%s/%s", path, entry->d_name);
 
-        printf("%s\n", subPath);
+        printf("%s\n", recursive ? subPath : entry->d_name);
 
-        if (entry->d_type == DT_DIR) {
-            recursivePrint(subPath);
+        if (recursive && entry->d_type == DT_DIR) {
+            printDir(subPath, 1);
         }
 
         free(namelist[i]);
@@ -82,20 +46,13 @@ void recursivePrint(const char *path) {
 }
 
 int main(int argc, char *argv[]) {
-    const char *path;
-
     if (argc > 2) {
         fprintf(stderr, "tuls: too many arguments\n");
         exit(1);
     }
 
-    path = (argc > 1) ? argv[1] : ".";
-
-    if (argc > 1) {
-        recursivePrint(path);
-    } else {
-        printDir(path);
-    }
+    const char *path = (argc > 1) ? argv[1] : ".";
+    printDir(path, argc > 1);
 
     return 0;
 }
